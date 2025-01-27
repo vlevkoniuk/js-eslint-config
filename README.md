a3e4a6b1872bd479f6699f474fa10ecde8d133af
Viacheslav Levkoniuk <viacheslav.levkoniuk@angloamerican.com>
2025-01-27 12:32:12 +0200

    added unicorn esrule for kebab case file namings

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ recommended config from me for programming on a JavaScript
 
 In order to use this config you have to install the following NPM packages
 ```
-npm i -D @eslint/js eslint globals
+npm i -D @eslint/js eslint globals eslint-plugin-unicorn
 ```
 
 Then put eslint.config.mjs and .prettierrc into your base directory
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,45 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{
-		languageOptions: { globals: globals.browser }
-	},
-	pluginJs.configs.recommended,
-	{
+    {
+        languageOptions: { globals: globals.browser }
+    },
+
+    pluginJs.configs.recommended,
+    {
+        plugins: {
+            unicorn: eslintPluginUnicorn
+        },
         name: 'best practices rules',
-        files: ["**/*.js"],
-		rules: {
-			...pluginJs.configs.recommended.rules,
-			semi: ["error", "always"],
-			indent: ['error', 4],
-			'max-len': [
+        files: ['**/*.js'],
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            semi: ['error', 'always'],
+            indent: ['error', 4],
+            'max-len': [
                 'warn',
                 {
                     code: 140,
                     ignoreUrls: true,
                     ignoreStrings: true,
-                    ignoreTemplateLiterals: true,
+                    ignoreTemplateLiterals: true
                 }
-			],
-			'brace-style': ['warn'],
-			'comma-dangle': ['error', 'never'],
+            ],
+            'brace-style': ['warn'],
+            'comma-dangle': ['error', 'never'],
             'no-multiple-empty-lines': ['error', { max: 2 }],
-            'no-trailing-spaces': ['error', { 'skipBlankLines': false }],
-            'eol-last': ['error', 'always']
-		}
+            'no-trailing-spaces': ['error', { skipBlankLines: false }],
+            'eol-last': ['error', 'always'],
+            'unicorn/filename-case': [
+                'error',
+                {
+                    case: 'kebabCase'
+                }
+            ]
+        }
     }
 ];
